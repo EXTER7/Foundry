97f01f51458bd276a9ce24bb44d2b8b25ea53cbc
EXTER <exter7@gmail.com>
2013-08-18 02:29:42 -0400

    Put API files in the mod package.

--- build_foundry.sh
+++ build_foundry.sh
@@ -7,8 +7,16 @@ if ./recompile.sh && ./reobfuscate.sh
 then
   mkdir -p "packed/exter"
   mkdir -p "packed/assets/"$MODNAME
+
+  mkdir -p "packed/buildcraft"
+
   cp -r "reobf/minecraft/exter/"$MODNAME "packed/exter/"
   cp -r "src/minecraft/assets/"$MODNAME"/"* "packed/assets/$MODNAME/"
+
+  cp -r "reobf/minecraft/buildcraft/api" "packed/buildcraft/"
+  cp -r "reobf/minecraft/forestry" "packed/forestry/"
+  cp -r "reobf/minecraft/ic2" "packed/ic2/"
+
   cd packed
   zip -r $MODNAME".zip" *
   mv $MODNAME".zip" "../"

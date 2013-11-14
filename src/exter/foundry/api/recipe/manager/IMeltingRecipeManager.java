cf14f72ac26cf52a73dd404a0420533751602d75
EXTER <exter7@gmail.com>
2013-11-14 03:31:30 -0400

    Added missing recipe API method.

--- src/exter/foundry/api/recipe/manager/IMeltingRecipeManager.java
+++ src/exter/foundry/api/recipe/manager/IMeltingRecipeManager.java
@@ -12,11 +12,18 @@ public interface IMeltingRecipeManager
   
   /**
    * Register a Metal Smelter recipe
-   * @param solid_name Item to be melted
+   * @param solid Ore Dictionary name of the item to be melted
    * @param fluid_stack Resulting fluid
    */
-  public void AddRecipe(String solid_name,FluidStack fluid_stack);
+  public void AddRecipe(String solid,FluidStack fluid_stack);
   
+  /**
+   * Register a Metal Smelter recipe
+   * @param solid Item to be melted
+   * @param fluid_stack Resulting fluid
+   */
+  public void AddRecipe(ItemStack solid,FluidStack fluid_stack);
+
   /**
    * Get a list of all the recipes
    * @return List of all the recipes

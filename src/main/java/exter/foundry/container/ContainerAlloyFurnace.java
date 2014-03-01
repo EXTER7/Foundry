90378ab5aedadab1937031bdf8ab2427075e0352
EXTER <exter7@gmail.com>
2014-03-01 02:46:19 -0400

    Implement Alloy Furnace GUI.

---
+++ src/main/java/exter/foundry/container/ContainerAlloyFurnace.java
@@ -0,0 +1,113 @@
+package exter.foundry.container;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.Container;
+import net.minecraft.inventory.Slot;
+import net.minecraft.inventory.SlotFurnace;
+import net.minecraft.item.ItemStack;
+import exter.foundry.slot.SlotContainer;
+import exter.foundry.tileentity.TileEntityAlloyFurnace;
+
+public class ContainerAlloyFurnace extends Container
+{
+  
+
+  private TileEntityAlloyFurnace te_alloyfurnace;
+  
+  // Slot numbers
+  private static final int SLOTS_TE = 0;
+  //private static final int SLOTS_TE_SIZE = 4;
+  
+  private static final int SLOTS_INVENTORY = 4;
+  private static final int SLOTS_HOTBAR = 4 + 3 * 9;
+
+  private static final int SLOT_INVENTORY_X = 8;
+  private static final int SLOT_INVENTORY_Y = 84;
+
+  private static final int SLOT_HOTBAR_X = 8;
+  private static final int SLOT_HOTBAR_Y = 142;
+
+  public ContainerAlloyFurnace(TileEntityAlloyFurnace furnace, EntityPlayer player)
+  {
+    te_alloyfurnace = furnace;
+    te_alloyfurnace.openInventory();
+    int i,j;
+
+    
+    addSlotToContainer(new SlotContainer(te_alloyfurnace,TileEntityAlloyFurnace.SLOT_INPUT_A,38,17));
+    addSlotToContainer(new SlotContainer(te_alloyfurnace,TileEntityAlloyFurnace.SLOT_INPUT_B,56,17));
+    addSlotToContainer(new SlotFurnace(player,te_alloyfurnace,TileEntityAlloyFurnace.SLOT_OUTPUT,116,35));
+    addSlotToContainer(new SlotContainer(te_alloyfurnace,TileEntityAlloyFurnace.SLOT_FUEL,48,53));
+
+    //Player Inventory
+    for(i = 0; i < 3; ++i)
+    {
+      for(j = 0; j < 9; ++j)
+      {
+        addSlotToContainer(new Slot(player.inventory, j + i * 9 + 9, SLOT_INVENTORY_X + j * 18, SLOT_INVENTORY_Y + i * 18));
+      }
+    }
+    for(i = 0; i < 9; ++i)
+    {
+      addSlotToContainer(new Slot(player.inventory, i, SLOT_HOTBAR_X + i * 18, SLOT_HOTBAR_Y));
+    }
+  }
+
+  public boolean canInteractWith(EntityPlayer par1EntityPlayer)
+  {
+    return te_alloyfurnace.isUseableByPlayer(par1EntityPlayer);
+  }
+
+  public ItemStack transferStackInSlot(EntityPlayer player, int slot_index)
+  {
+    ItemStack slot_stack = null;
+    Slot slot = (Slot) inventorySlots.get(slot_index);
+
+    if (slot != null && slot.getHasStack())
+    {
+      ItemStack stack = slot.getStack();
+      slot_stack = stack.copy();
+
+      if (slot_index >= SLOTS_INVENTORY && slot_index < SLOTS_HOTBAR)
+      {
+        if(!mergeItemStack(stack, SLOTS_TE, SLOTS_TE + TileEntityAlloyFurnace.SLOT_INPUT_B, false))
+        {
+          return null;
+        }
+      } else if (slot_index >= SLOTS_HOTBAR && slot_index < SLOTS_HOTBAR + 9)
+      {
+        if (!mergeItemStack(stack, SLOTS_INVENTORY, SLOTS_INVENTORY + 3 * 9, false))
+        {
+          return null;
+        }
+      } else if (!mergeItemStack(stack, SLOTS_INVENTORY, SLOTS_HOTBAR + 9, false))
+      {
+        return null;
+      }
+
+      if (stack.stackSize == 0)
+      {
+        slot.putStack((ItemStack) null);
+      } else
+      {
+        slot.onSlotChanged();
+      }
+
+      if (stack.stackSize == slot_stack.stackSize)
+      {
+        return null;
+      }
+
+      slot.onPickupFromSlot(player, stack);
+    }
+
+    return slot_stack;
+  }
+
+  @Override
+  public void onContainerClosed(EntityPlayer par1EntityPlayer)
+  {
+    super.onContainerClosed(par1EntityPlayer);
+    te_alloyfurnace.closeInventory();
+  }
+}
---
+++ src/main/java/exter/foundry/gui/GuiAlloyFurnace.java
@@ -0,0 +1,84 @@
+package exter.foundry.gui;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.ResourceLocation;
+
+import org.lwjgl.opengl.GL11;
+
+import exter.foundry.container.ContainerAlloyFurnace;
+import exter.foundry.tileentity.TileEntityAlloyFurnace;
+
+public class GuiAlloyFurnace extends GuiFoundry
+{
+
+  public static final ResourceLocation GUI_TEXTURE = new ResourceLocation("foundry:textures/gui/alloyfurnace.png");
+
+  public static final int BURN_X = 49;
+  public static final int BURN_Y = 37;
+  public static final int BURN_WIDTH = 14;
+  public static final int BURN_HEIGHT = 14;
+
+  public static final int PROGRESS_X = 80;
+  public static final int PROGRESS_Y = 34;
+  public static final int PROGRESS_WIDTH = 22;
+  public static final int PROGRESS_HEIGHT = 15;
+
+  public static final int PROGRESS_OVERLAY_X = 176;
+  public static final int PROGRESS_OVERLAY_Y = 14;
+
+  public static final int BURN_OVERLAY_X = 176;
+  public static final int BURN_OVERLAY_Y = 14;
+
+
+  private TileEntityAlloyFurnace te_af;
+
+  public GuiAlloyFurnace(TileEntityAlloyFurnace ms, EntityPlayer player)
+  {
+    super(new ContainerAlloyFurnace(ms, player));
+    allowUserInput = false;
+    ySize = 166;
+    te_af = ms;
+  }
+
+  @Override
+  protected void drawGuiContainerForegroundLayer(int mouse_x, int mouse_y)
+  {
+    super.drawGuiContainerForegroundLayer(mouse_x, mouse_y);
+
+    fontRendererObj.drawString("Alloy Furnace", 5, 6, 0x404040);
+    fontRendererObj.drawString("Inventory", 8, (ySize - 96) + 2, 0x404040);
+
+  }
+
+  @Override
+  protected void drawGuiContainerBackgroundLayer(float f, int x, int y)
+  {
+    GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+    mc.renderEngine.bindTexture(GUI_TEXTURE);
+    int window_x = (width - xSize) / 2;
+    int window_y = (height - ySize) / 2;
+    drawTexturedModalRect(window_x, window_y, 0, 0, xSize, ySize);
+
+    
+    if(te_af.item_burn_time > 0)
+    {
+      int burn = te_af.burn_time * PROGRESS_HEIGHT / te_af.item_burn_time;
+
+      if(burn > 0)
+      {
+        drawTexturedModalRect(window_x + BURN_X, window_y + BURN_Y + BURN_HEIGHT - burn, BURN_OVERLAY_X, BURN_OVERLAY_Y + BURN_HEIGHT - burn, BURN_WIDTH, burn);
+      }
+    }
+    if(te_af.progress > 0)
+    {
+      int progress = te_af.progress * PROGRESS_WIDTH / 400;
+      drawTexturedModalRect(window_x + PROGRESS_X, window_y + PROGRESS_Y, PROGRESS_OVERLAY_X, PROGRESS_OVERLAY_Y, progress, PROGRESS_HEIGHT);
+    }
+  }
+
+  @Override
+  protected ResourceLocation GetGUITexture()
+  {
+    return GUI_TEXTURE;
+  }
+}

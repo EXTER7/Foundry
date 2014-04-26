eea16c14e1df496f1a8afc95d13e074b478adc27
EXTER <exter7@gmail.com>
2014-04-26 18:44:51 -0400

    Release 0.6.2.2

--- src/main/java/exter/foundry/ModFoundry.java
+++ src/main/java/exter/foundry/ModFoundry.java
@@ -85,7 +85,7 @@ public class ModFoundry
 {
   public static final String MODID = "foundry";
   public static final String MODNAME = "Foundry";
-  public static final String MODVERSION = "0.6.2.1";
+  public static final String MODVERSION = "0.6.2.2";
 
   public static final String CHANNEL = "EXTER_Foundry";
 

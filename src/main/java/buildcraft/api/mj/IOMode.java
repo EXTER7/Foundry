54a3b131944db9a46f7853e1b3b4b43945579c58
EXTER <exter7@gmail.com>
2014-05-26 23:22:08 -0400

    Updated Buildcraft API.

--- src/main/java/buildcraft/api/mj/IOMode.java
+++ src/main/java/buildcraft/api/mj/IOMode.java
@@ -6,6 +6,15 @@
  * License 1.0, or MMPL. Please check the contents of the license located in
  * http://www.mod-buildcraft.com/MMPL-1.0.txt
  */
-@API(apiVersion = "1.1", owner = "BuildCraftAPI|core", provides = "BuildCraftAPI|power")
-package buildcraft.api.power;
-import cpw.mods.fml.common.API;
+package buildcraft.api.mj;
+
+public enum IOMode {
+	Both(true, true), Receive(true, false), Send(false, true), None(false, false);
+
+	public final boolean canReceive, canSend;
+
+	IOMode(boolean canReceive, boolean canSend) {
+		this.canReceive = canReceive;
+		this.canSend = canSend;
+	}
+}

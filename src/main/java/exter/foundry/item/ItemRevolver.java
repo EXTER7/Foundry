ac8a3748550e9ebf2092582c8ed44d23405ee41f
EXTER <exter7@gmail.com>
2015-06-29 22:58:25 -0400

    Tweaked the revolver texture and added a new gunshot sound.

--- src/main/java/exter/foundry/item/ItemRevolver.java
+++ src/main/java/exter/foundry/item/ItemRevolver.java
@@ -14,7 +14,6 @@ import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.Items;
 import net.minecraft.item.EnumAction;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;

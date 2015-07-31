657e97a4dd936170559ddfedb6c61ca03a63540d
EXTER <exter7@gmail.com>
2015-07-31 11:21:36 -0400

    1.1.0.0 Beta Release 1

--- src/main/java/exter/foundry/ModFoundry.java
+++ src/main/java/exter/foundry/ModFoundry.java
@@ -85,7 +85,7 @@ public class ModFoundry
 {
   public static final String MODID = "foundry";
   public static final String MODNAME = "Foundry";
-  public static final String MODVERSION = "1.1.0.0-pre";
+  public static final String MODVERSION = "1.1.0.0-beta1";
 
   @Instance(MODID)
   public static ModFoundry instance;

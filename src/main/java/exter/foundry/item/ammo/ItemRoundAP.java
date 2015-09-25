c304b244ae5dad4d38db55c74b2376a39d54b281
EXTER <exter7@gmail.com>
2015-09-25 18:14:21 -0400

    Added missing files to repository.

---
+++ src/main/java/exter/foundry/item/ammo/ItemRoundAP.java
@@ -0,0 +1,46 @@
+package exter.foundry.item.ammo;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import exter.foundry.item.firearm.ItemRevolver;
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.IIcon;
+
+public class ItemRoundAP extends ItemRoundBase
+{
+  public IIcon icon;
+  
+  public ItemRoundAP()
+  {
+    super(7,50,25);
+    setUnlocalizedName("roundAP");
+  }
+
+
+  @Override
+  @SideOnly(Side.CLIENT)
+  public void registerIcons(IIconRegister register)
+  {
+    icon = register.registerIcon("foundry:round_ap");
+  }
+
+  @Override
+  @SideOnly(Side.CLIENT)
+  public IIcon getIconFromDamage(int dmg)
+  {
+    return icon;
+  }
+  
+  @Override
+  public String GetRoundType(ItemStack round)
+  {
+    return ItemRevolver.AMMO_TYPE;
+  }
+
+  @Override
+  public boolean IgnoresArmor(ItemStack round)
+  {
+    return true;
+  }
+}
---
+++ src/main/java/exter/foundry/item/ammo/ItemShellAP.java
@@ -0,0 +1,52 @@
+package exter.foundry.item.ammo;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import exter.foundry.item.FoundryItems;
+import exter.foundry.item.ItemComponent;
+import exter.foundry.item.firearm.ItemShotgun;
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.IIcon;
+
+public class ItemShellAP extends ItemRoundBase
+{
+  public IIcon icon;
+  public ItemShellAP()
+  {
+    super(4,40,15);
+    setUnlocalizedName("shellAP");
+  }
+
+  @Override
+  @SideOnly(Side.CLIENT)
+  public void registerIcons(IIconRegister register)
+  {
+    icon = register.registerIcon("foundry:shell_ap");
+  }
+
+  @Override
+  @SideOnly(Side.CLIENT)
+  public IIcon getIconFromDamage(int dmg)
+  {
+    return icon;
+  }
+  
+  @Override
+  public String GetRoundType(ItemStack round)
+  {
+    return ItemShotgun.AMMO_TYPE;
+  }
+
+  @Override
+  public ItemStack GetCasing(ItemStack round)
+  {
+    return FoundryItems.Component(ItemComponent.COMPONENT_AMMO_CASING_SHELL);
+  }
+  
+  @Override
+  public boolean IgnoresArmor(ItemStack round)
+  {
+    return true;
+  }
+}

68b9c1f5904a69f6fadc9ec5990616964bd6a1af
EXTER <exter7@gmail.com>
2015-10-07 19:32:46 -0400

    Setup dev environment for MC-1.8

--- src/api/java/com/teammetallurgy/metallurgy/api/MetalType.java
+++
@@ -1,6 +0,0 @@
-package com.teammetallurgy.metallurgy.api;
-
-public enum MetalType
-{
-    Respawn, Ore, Catalyst, Alloy, Drop, Default, Unknown
-}
--- src/api/java/com/teammetallurgy/metallurgy/api/package-info.java
+++
@@ -1,4 +0,0 @@
-@API(owner="Metallurgy", apiVersion = "2.0.0", provides="MetallurgyAPI")
-package com.teammetallurgy.metallurgy.api;
-
-import cpw.mods.fml.common.API;

0fa9eadbeb7b7a922f779113a6634d8dc2c9839a
EXTER <exter7@gmail.com>
2015-10-22 14:49:46 -0400

    Added tooltips to AP ammo.

--- src/main/java/exter/foundry/item/ammo/ItemRoundAP.java
+++ src/main/java/exter/foundry/item/ammo/ItemRoundAP.java
@@ -1,7 +1,14 @@
 package exter.foundry.item.ammo;
 
+import java.util.List;
+
 import exter.foundry.item.firearm.ItemRevolver;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumChatFormatting;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class ItemRoundAP extends ItemRoundBase
 {
@@ -21,5 +28,17 @@ public class ItemRoundAP extends ItemRoundBase
   public boolean ignoresArmor(ItemStack round)
   {
     return true;
+  }
+
+  @SuppressWarnings("unchecked")
+  @Override
+  @SideOnly(Side.CLIENT)
+  public void addInformation(ItemStack stack, EntityPlayer player, @SuppressWarnings("rawtypes") List list, boolean par4)
+  {
+    super.addInformation(stack, player, list, par4);
+    if(GuiScreen.isShiftKeyDown())
+    {
+      list.add(EnumChatFormatting.YELLOW + "Bypasses armor.");
+    }
   }
 }
--- src/main/java/exter/foundry/item/ammo/ItemShellAP.java
+++ src/main/java/exter/foundry/item/ammo/ItemShellAP.java
@@ -1,9 +1,16 @@
 package exter.foundry.item.ammo;
 
+import java.util.List;
+
 import exter.foundry.item.FoundryItems;
 import exter.foundry.item.ItemComponent;
 import exter.foundry.item.firearm.ItemShotgun;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumChatFormatting;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class ItemShellAP extends ItemRoundBase
 {
@@ -29,5 +36,17 @@ public class ItemShellAP extends ItemRoundBase
   public boolean ignoresArmor(ItemStack round)
   {
     return true;
+  }
+  
+  @SuppressWarnings("unchecked")
+  @Override
+  @SideOnly(Side.CLIENT)
+  public void addInformation(ItemStack stack, EntityPlayer player, @SuppressWarnings("rawtypes") List list, boolean par4)
+  {
+    super.addInformation(stack, player, list, par4);
+    if(GuiScreen.isShiftKeyDown())
+    {
+      list.add(EnumChatFormatting.YELLOW + "Bypasses armor.");
+    }
   }
 }

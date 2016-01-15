ee9bc34a6ee8a948b7508f7d228eaa8bb2bd4165
EXTER <exter7@gmail.com>
2016-01-15 01:50:59 -0400

    Release 1.3.3.0

--- src/main/java/exter/foundry/ModFoundry.java
+++ src/main/java/exter/foundry/ModFoundry.java
@@ -71,7 +71,7 @@ public class ModFoundry
 {
   public static final String MODID = "foundry";
   public static final String MODNAME = "Foundry";
-  public static final String MODVERSION = "1.3.2.0";
+  public static final String MODVERSION = "1.3.3.0";
 
   @Instance(MODID)
   public static ModFoundry instance;

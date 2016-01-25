71ffc46d5c66c485960e72fa5adf0f00be211247
EXTER <exter7@gmail.com>
2016-01-25 17:18:52 -0400

    Prerelease 1.4.0.0-alpha1

--- src/main/java/exter/foundry/ModFoundry.java
+++ src/main/java/exter/foundry/ModFoundry.java
@@ -68,7 +68,7 @@ public class ModFoundry
 {
   public static final String MODID = "foundry";
   public static final String MODNAME = "Foundry";
-  public static final String MODVERSION = "1.3.3.1";
+  public static final String MODVERSION = "1.4.0.0-alpha1";
 
   @Instance(MODID)
   public static ModFoundry instance;
